515fceaceedd4737c58b8e841a2463995c11fb51
Surya <suryathegreat.07@gmail.com>
2020-04-14 14:17:15 +0530

    Initial commit

---
+++ src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -0,0 +1,36 @@
+package com.swanoff.aopdemo.aspect;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class MyDemoLoggingAspect {
+	
+	@Pointcut("execution(* com.swanoff.aopdemo.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	// create pointcut for getter methods
+	@Pointcut("execution(* com.swanoff.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.swanoff.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// create pointcut: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNotGetterSetter() {}
+	
+	@Before("forDaoPackageNotGetterSetter()")
+	public void beforeAddAccountAdvice() {
+		System.out.println("\n=====> Executing @Before advice on method");
+	}
+	
+	@Before("forDaoPackageNotGetterSetter()")
+	public void performApiAnalytics() {
+		System.out.println("\n=====> Performing API analytics");
+	}
+}
