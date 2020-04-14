11199a2b4f0fe16a340fb36082eabb48a8208428
Surya <suryathegreat.07@gmail.com>
2020-04-14 15:34:01 +0530

    Ordered the aspect sequence using @Order
    lower value - high priority

--- src/main/java/com/swanoff/aopdemo/aspect/MyApiAnalyticsAspect.java
+++ src/main/java/com/swanoff/aopdemo/aspect/MyApiAnalyticsAspect.java
@@ -2,10 +2,12 @@ package com.swanoff.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(3)
 public class MyApiAnalyticsAspect {
 	
 	@Before("com.swanoff.aopdemo.aspect.AopExpressions.forDaoPackageNotGetterSetter()")
--- src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/main/java/com/swanoff/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -2,11 +2,12 @@ package com.swanoff.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
+@Order(2)
 public class MyDemoLoggingAspect {
 	
 	@Before("com.swanoff.aopdemo.aspect.AopExpressions.forDaoPackageNotGetterSetter()")
